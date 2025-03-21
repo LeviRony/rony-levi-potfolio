37a80723749a1785aba98989e1f478dd64aa07e4
RonyBoom <ronylevi99@gmail.com>
2025-03-21 11:03:08 +0200

    sync

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const itemRoutes = require('./itemRoutes');
-const customerRoutes = require('./customerRoutes');
-const productRoutes = require('./productRoutes');
 const reviewRoutes = require('./reviewRoutes.js');
-const orderRoutes = require('./orderRoutes');
 
-router.use('/items', itemRoutes);
-router.use('/customers', customerRoutes);
-router.use('/products', productRoutes);
+
+
 router.use('/reviews', reviewRoutes);
-router.use('/orders', orderRoutes);
+
 
 
 
